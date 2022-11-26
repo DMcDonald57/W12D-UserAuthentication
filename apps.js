112e723dce7b43e918d4535dbd7b8824637caba4
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-25 23:13:24 -0700

    created the home page after login and drafted login functions for testing

--- apps.js
+++ apps.js
@@ -5,8 +5,20 @@ function userLogin() {
         url : `https://reqres.in/`,
         method : `POST`,
         data : {
-            email : document.getElementById
+            email : document.getElementById(`userlogin`),
+            password : document.getElementById(`userPassword`),
         }
-    })
+    }).then(loginSuccessful).catch(loginFailed);
+}
+
+function loginSuccessful() {
+    console.log(`success`);
+    Cookies.set(`sessionToken`, respons.data.token);
+    document.body(<a href="home.html"></a>);
+}
+
+function loginFailed(error) {
+    console.log(error.response.data.error);
+    alert(error.response.data.error);
     
 }
