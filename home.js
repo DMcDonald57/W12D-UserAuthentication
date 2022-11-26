578c86eee77cf790e1359d5dedeec47cd9693b48
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-26 13:44:21 -0700

    home JS functions working as expected except for color in a box.

--- home.js
+++ home.js
@@ -1,11 +1,18 @@
 axios.request({
     url : `https://reqres.in/api/unknown`
-}).then().catch();
+}).then(colors).catch(nocolors);
 
 function colors(response) {
-    let data = response.data;
+    let data = response.data.data;
     for (let post of data){
-        document
+        document.body.insertAdjacentHTML(`beforeend`, `<h3>Name:${post.name}</h3>`);
+        document.body.insertAdjacentHTML(`beforeend`, `<h3>Year:${post.year}</h3>`);
+        
+
+        // document.body.insertAdjacentHTML(`beforeend`, `${post.color}`);
     }
-    
+}
+
+function nocolors(error) {
+    document.body.insertAdjacentHTML(`afterend`, `<h2>No colors for you.</h2>`);
 }
